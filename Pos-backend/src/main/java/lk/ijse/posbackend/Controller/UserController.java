fb566a6b5ac3fdbc04ded6a729c3024a7d0e0faa
pavithrani <pererapavithrani02@gmail.com>
2024-10-07 19:11:19 +0530

    implement user registration

---
+++ Pos-backend/src/main/java/lk/ijse/posbackend/Controller/UserController.java
@@ -0,0 +1,34 @@
+package lk.ijse.posbackend.Controller;
+
+import lk.ijse.posbackend.Dto.UserDto;
+import lk.ijse.posbackend.Exceptions.DataPersistException;
+import lk.ijse.posbackend.Mapping.Mapping;
+import lk.ijse.posbackend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+
+    }
+     @PostMapping
+    public ResponseEntity<String> registerUser( @RequestBody UserDto userDto) {
+        try{
+          userService.saveUser(userDto);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
--- Pos-backend/src/main/java/lk/ijse/posbackend/Service/impl/UserServiceImpl.java
+++ Pos-backend/src/main/java/lk/ijse/posbackend/Service/impl/UserServiceImpl.java
@@ -1,15 +1,30 @@
 package lk.ijse.posbackend.Service.impl;
 
+import lk.ijse.posbackend.Dao.UserDao;
 import lk.ijse.posbackend.Dto.UserDto;
+import lk.ijse.posbackend.Entity.UserEntity;
+import lk.ijse.posbackend.Exceptions.DataPersistException;
+import lk.ijse.posbackend.Mapping.Mapping;
 import lk.ijse.posbackend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
-
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private Mapping userMapping;
     @Override
     public void saveUser(UserDto userDto) {
-
+        UserEntity saved = userDao.save(userMapping.toUserEntity(userDto));
+        if (saved==null){
+            throw new DataPersistException("User Not Registered");
+        }
     }
 
     @Override
