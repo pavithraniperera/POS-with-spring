571320d4ae90cfcc28b260052c409236471c88de
pavithrani <pererapavithrani02@gmail.com>
2024-10-07 07:52:06 +0530

    Create OrderItemDto class

---
+++ Pos-backend/src/main/java/lk/ijse/posbackend/Dto/OrderItemDto.java
@@ -0,0 +1,21 @@
+package lk.ijse.posbackend.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDto {
+    private Long id;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+    private int itemId;
+    private String itemName;
+
+
+}
