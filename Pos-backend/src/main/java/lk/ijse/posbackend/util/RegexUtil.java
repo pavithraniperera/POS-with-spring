3e0764bddd9892c9fa0dfba1df8196209402f091
pavithrani <pererapavithrani02@gmail.com>
2024-10-09 11:08:04 +0530

    update RegexUtil

--- Pos-backend/src/main/java/lk/ijse/posbackend/util/RegexUtil.java
+++ Pos-backend/src/main/java/lk/ijse/posbackend/util/RegexUtil.java
@@ -5,9 +5,15 @@ import java.util.regex.Pattern;
 public class RegexUtil {
     // Define the regex pattern for customer ID (e.g., C001)
     private static final String CUSTOMER_ID_REGEX = "^C\\d{3}$";
+    private static final String ITEM_ID_REGEX = "^ITEM-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+
 
     // Utility method to validate customer ID
     public static boolean isValidCustomerId(String customerId) {
         return Pattern.matches(CUSTOMER_ID_REGEX, customerId);
     }
+    public static boolean isValidItemId(String itemId) {
+        return Pattern.matches(ITEM_ID_REGEX, itemId);
+    }
+
 }
