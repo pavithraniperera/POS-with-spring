8a559fe345d5ad51f7f11a1749996998087807ce
pavithrani <pererapavithrani02@gmail.com>
2024-10-14 19:22:23 +0530

    added User class Logging

--- Pos-backend/src/main/java/lk/ijse/posbackend/Controller/UserController.java
+++ Pos-backend/src/main/java/lk/ijse/posbackend/Controller/UserController.java
@@ -3,6 +3,8 @@ package lk.ijse.posbackend.Controller;
 import lk.ijse.posbackend.Dto.impl.UserDto;
 import lk.ijse.posbackend.Exceptions.DataPersistException;
 import lk.ijse.posbackend.Service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import java.util.List;
 @CrossOrigin
 @RequestMapping("api/v1/users")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     @Autowired
@@ -25,11 +28,12 @@ public class UserController {
     public ResponseEntity<String> registerUser( @RequestBody UserDto userDto) {
         try{
           userService.saveUser(userDto);
+
         }catch (DataPersistException e){
             e.printStackTrace();
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-
+         logger.info("saved User successfully");
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
